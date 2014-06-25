2367b81f632155c1889006a14088792b2c989ce1
Mingli Yuan <mingli.yuan@gmail.com>
2014-06-25 17:00:12 +0800

    by using handlers

--- src/wikicrawl/cat.clj
+++ src/wikicrawl/cat.clj
@@ -2,6 +2,8 @@
   (:use [wikicrawl.config]
         [wikicrawl.util]))
 
+(def handlers (atom {}))
+
 (defn traverse-tree [lang page tree path depth progress]
   (Thread/sleep (* (+ 400 (rand-int 300)) @counter))
   (let [filename (to-file-name lang page)
@@ -11,33 +13,46 @@
       (cond
         (category? lang page)
           (when (and (>= depth 0) (< progress 10))
-            (println (str ".. " curpath))
-            (clojure.java.io/make-parents curpath)
-            (.mkdir curpath)
-            (let [subcats (query-subcat lang page)
-                  articles (query-article lang page)]
-              (with-open [newfile (java.io.FileWriter.
-                           (java.io.File. curpath "_Category"))]
-                (if-let [text (gen-content lang page tree)]
-                  (.write newfile text)))
-              (doseq [article articles]
-                (traverse-tree lang article newtree curpath depth progress))
-              (doseq [subcat subcats]
-                (if (< (count articles) 2)
-                  (traverse-tree lang subcat newtree curpath depth
-                                 (inc progress))
-                  (traverse-tree lang subcat newtree curpath (dec depth)
-                                 (inc progress))))))
+            ((:dir @handlers) lang page tree path depth progress))
       (not (specials? page))
         (when (zero? (.length curpath))
-          (do
-            (clojure.java.io/make-parents curpath)
-            (if (not (.exists curpath))
-              (with-open [newfile (java.io.FileWriter. curpath)]
-                (println (str "-> " curpath))
-                (if-let [text (gen-content lang page tree)]
-                    (.write newfile text)))
-                  (println (str "-- " curpath)))))))))
+          ((:file @handlers) lang page tree path depth progress))))))
+
+(defn handle-dir [lang page tree path depth progress]
+  (let [filename (to-file-name lang page)
+        curpath (java.io.File. path filename)
+        newtree (conj tree (to-name page))]
+    (println (str ".. " curpath))
+    (clojure.java.io/make-parents curpath)
+    (.mkdir curpath)
+    (let [subcats (query-subcat lang page)
+          articles (query-article lang page)]
+      (with-open [newfile (java.io.FileWriter.
+        (java.io.File. curpath "_Category"))]
+        (if-let [text (gen-content lang page tree)]
+          (.write newfile text)))
+      (doseq [article articles]
+        (traverse-tree lang article newtree curpath depth progress))
+      (doseq [subcat subcats]
+        (if (< (count articles) 2)
+          (traverse-tree lang subcat newtree curpath depth
+                         (inc progress))
+          (traverse-tree lang subcat newtree curpath (dec depth)
+                         (inc progress)))))))
+
+(defn handle-file [lang page tree path depth progress]
+  (let [filename (to-file-name lang page)
+        curpath (java.io.File. path filename)]
+    (clojure.java.io/make-parents curpath)
+    (if (not (.exists curpath))
+      (with-open [newfile (java.io.FileWriter. curpath)]
+        (println (str "-> " curpath))
+        (if-let [text (gen-content lang page tree)]
+          (.write newfile text)))
+      (println (str "-- " curpath)))))
+
+(swap! handlers assoc :dir handle-dir)
+(swap! handlers assoc :file handle-file)
 
 (defn crawl-cat [lang]
   (let [lang-root-path (java.io.File. root-path (name lang))]
