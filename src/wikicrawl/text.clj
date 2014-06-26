47ddfa33f5b13313fb29d01a22e2b949f69cf268
jseagull <jonathon.seagull@yahoo.com.cn>
2014-06-26 15:29:26 +0800

    fix content crawl

--- src/wikicrawl/text.clj
+++ src/wikicrawl/text.clj
@@ -4,7 +4,6 @@
   (:require [clj-yaml.core :as yaml]))
 
 (defn crawl-file [lang file]
-  (Thread/sleep (* (+ 400 (rand-int 200)) @counter))
   (try
     (let [pagename (lang (apply merge (:Names (yaml/parse-string
                                                 (slurp file)))))
@@ -12,22 +11,20 @@
           newname (str (.substring path 0 (inc (.lastIndexOf path ".")))
                        "txt")]
       (if (nil? pagename) nil
-        (with-open [newfile (java.io.FileWriter. newname)]
-          (if-let [text (gen-page lang pagename)]
-            (do
-              (println "->" pagename)
-              (.write newfile text))))))
-    (catch Throwable e (println e))))
+        (if (zero? (.length (java.io.File. newname)))
+          (with-open [newfile (java.io.FileWriter. newname)]
+            (if-let [text (gen-page lang pagename)]
+              (do
+                (println "->" newname)
+                (.write newfile text)))))))
+      (catch Throwable e (.printStackTrace e))))
 
 (defn crawl-dir [lang dir]
-  (for [file (.listFiles dir)]
+  (doall (doseq [file (.listFiles dir)]
     (if (.isFile file)
       (if (.endsWith (.getName file) ".yaml")
-        (.start (Thread. (fn []
-          (swap! counter inc)
-          (crawl-file lang file)
-          (swap! counter dec)))))
-      (crawl-dir lang file))))
+          (crawl-file lang file))
+      (crawl-dir lang file)))))
 
 (defn crawl-text [lang]
   (let [lang-root-path (java.io.File. root-path (name lang))]
